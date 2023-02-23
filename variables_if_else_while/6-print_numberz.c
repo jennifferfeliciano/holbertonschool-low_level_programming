b2db6b060f522ef938bd61bb1ecf1f7c1fa62c8c
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-23 03:24:58 +0000

    Write a program that prints all single digit numbers of base 10 (using putchar)

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- Entry point
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
