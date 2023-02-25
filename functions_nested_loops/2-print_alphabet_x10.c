e6be8cdfa344839bbfee908d1e1ec76288316757
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-25 21:35:15 +0000

    program that prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *main- Entry point
+ *Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int  counter = 1;
+       	char character;
+
+	while (counter <= 10)
+	{
+		character = 'a';
+		while	(character <= 'z')
+		{
+			_putchar(character);
+			character++;
+		}
+
+		counter++;
+		_putchar('\n');
+	}
+}
