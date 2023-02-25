740aca369c2e4d1cd4f5b015a7e4b806b3af620f
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-25 20:25:53 +0000

    add function prototype 'void print_alphabet(void)' to header file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main- Entry point
+ *Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char character = 'a';
+
+	while (character <= 'z')
+	{
+		_putchar(character);
+	}
+
+	_putchar('\n');
+}
