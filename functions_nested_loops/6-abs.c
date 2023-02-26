74ff376dc5c51a259e4b65f44810786fd846f459
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-26 03:07:41 +0000

    program that computes the absolute value of a integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_abs- return an absolute value
+ *n: numbrer to check for absolute value
+ *Return: absolute value of a number or 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
