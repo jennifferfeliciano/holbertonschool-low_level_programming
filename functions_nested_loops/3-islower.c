69f4aef7c0a574af5545013df5b16edb308fad4b
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-26 01:12:49 +0000

    fix description for return value

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  *_islower - function that checks for lowercase characters.
  *@c: character to check if it is lowercase
- *Return- 1 if the character is lowercase or 0 if it is not
+ *Return: 1 if the character is lowercase or 0 if it is not
  */
 
 int _islower(int c)
