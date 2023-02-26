12c8a9bb2a99ebff8179ec5516632b9c63c44cc6
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-26 01:11:22 +0000

    fix description for parameter 'c'

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_islower- function that checks for lowercase characters.
+ *_islower - function that checks for lowercase characters.
  *@c: character to check if it is lowercase
  *Return- 1 if the character is lowercase or 0 if it is not
  */
