e8d75f8cf295d02566a973ab21485422e28f705f
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-26 01:01:16 +0000

    fix description for parameter 'c'

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *_islower- function that checks for lowercase characters.
- *c- character to check if it is lowercase
+ *@c- character to check if it is lowercase
  *Return- 1 if the character is lowercase or 0 if it is not
  */
 
