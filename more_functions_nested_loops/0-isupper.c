5422a47eb57281ed965ed7a2e1cdfbd70de84e8a
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-27 02:10:42 +0000

    add @ to parameter documentation

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  *_isupper- function that checks for uppercase character
- *c: character to be checked
+ *@c: character to be checked
  *Return: 1 if the character is uppercase or 0, otherwise
  */
 
