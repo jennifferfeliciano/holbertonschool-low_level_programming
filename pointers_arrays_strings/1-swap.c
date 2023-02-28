1916a05e403482dbcf014c68fa3d9db91158b9d2
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-02-28 00:27:34 +0000

    delete comment about @c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,7 +3,6 @@
  *swap_int- function that swaps the value of two integers.
  *@a: first integer to be swapped
  *@b: second integer to be swapped
- *@c: use to temporarily save the value of one of the integers to be swapped.
  *Return: void
  */
 
