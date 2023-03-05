88abbe8e992f3d2b45b66cb913fa6ada4fc41eb8
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-05 16:19:51 +0000

    fix commas and spaces between numbers

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,45 +1,35 @@
 #include "main.h"
-
 /**
  *times_table- function that prints 9 times table starting with 0
  *Return: void
  */
-
 void times_table(void)
 {
 	int x, y, mult, first_digit, last_digit;
-
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 			mult = (x * y);
-
 			if (mult > 9)
 			{
-
 				first_digit = mult / 10;
 				last_digit = mult % 10;
 
-				_putchar(' ');
-				_putchar(first_digit + '0');
-				_putchar(last_digit + '0');
-
-				if (y < 9)
-				{
 					_putchar(',');
-				}
+					_putchar(' ');
+					_putchar(first_digit + '0');
+					_putchar(last_digit + '0');
 			}
 			else
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(mult + '0');
-
-				if (y < 9)
+				if (y > 0)
 				{
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
+				_putchar(mult + '0');
 			}
 		}
 		_putchar('\n');
