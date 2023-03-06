75bd66668e9135723bdacaa5c196878fc2f6f2fe
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-06 14:36:12 +0000

    betty style and documentation

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
+ *print_line- function that draws a straight line
+ *@n: number of lines to be printed
+ *Return: void
  */
 
 void print_line(int n)
-{	
+{
 	int counter = 0;
-	
-	for (;counter < n; counter++)
+
+	for (; counter < n; counter++)
 	{
-	
 		if (n > 0)
 		{
 			_putchar(95);
@@ -20,7 +20,6 @@ void print_line(int n)
 		{
 			_putchar('\n');
 		}
-	
 	}
 	_putchar('\n');
 }
