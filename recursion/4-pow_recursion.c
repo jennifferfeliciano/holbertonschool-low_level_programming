16765adceaf605edc28049441f977131caf16491
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-08 03:06:43 +0000

    fix documentation

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  *_pow_recursion- function that returns the value of x raised to the power of y
  *@x: base
  *@y: exponent
- *Return: factorial value 
+ *Return: factorial value
  */
 
 int _pow_recursion(int x, int y)
