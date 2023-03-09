d23e6f87d38965b3fa8e24423d5acd71a83623b1
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-09 02:23:51 +0000

    add prototypes

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper- function that checks for uppercase character
+ *@c: character to be checked
+ *Return: 1 if the character is uppercase or 0, otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
