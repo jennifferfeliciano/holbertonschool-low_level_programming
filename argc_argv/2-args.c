b7c1d55697b450f08a45871b9529a7a167bd9093
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-10 02:29:17 +0000

    program that prints all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: all arguments received
+ */
+
+int main(int argc, char** argv)
+{
+	int counter = 0;
+
+	for (; counter < argc; counter++)
+	{
+		printf("%s\n", argv[counter]);
+	}
+	return (0);
+}
