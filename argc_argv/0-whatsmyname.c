5dded2a09ff00466392fd4ec3f3108f6f8138a19
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-03-11 03:53:43 +0000

    fix documentation

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
 
 /**
  *main- Entry point
+ *@argc: argument counter
  *@argv: argument vector
  *Return: void
  */
 
-int main(int argc __attribute__((unused)), char** argv)
+int main(int argc __attribute__((unused)), char **argv)
 
 {
 	printf("%s\n", argv[0]);
