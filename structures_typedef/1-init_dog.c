4515ad91b85bb3c4771bb56c6f5d4bb72bf39626
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-07-18 18:53:10 +0000

    fix documentation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,14 +2,12 @@
 #include <stddef.h>
 
 /**
- *init_dog- structure that initializes a struct dog values
+ *init_dog- function that initializes a struct dog values
  *@d: Pointer to the structure
  *@name: pointer to the dog's name
  *@age: pointer to the dog's age
  *@owner: pointer to the dog owner's name
  *
- * Description: Structure that inizializes a struct dog with
- * the provided valuesof age, name and owner's name.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
