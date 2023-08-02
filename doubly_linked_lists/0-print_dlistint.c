334c2c086f3aceb97d63878deb6168f08076498b
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-08-02 20:17:23 +0000

    Create Readme file: Title, description of the project, about, learning objetives and project requirements

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ *print_dlistint- function that prints all the elements of a list.
+ *@h: pointer to the head of a list.
+ *Return: the number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+        size_t counter = 0;
+
+        while (h != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                counter++;
+        }
+
+        while (h != NULL && h->prev != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->prev;
+                counter++;
+        }
+
+        return (counter);
+}
+
