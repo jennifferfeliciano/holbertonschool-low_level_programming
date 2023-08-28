b039812b66e7cc241e567198d383df7d35657223
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-08-28 01:35:24 +0000

    move text_content from first if statement

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,18 +12,23 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, append_text;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_APPEND | O_RDWR);
 	if (fd == -1)
 		return (-1);
 
-	append_text = write(fd, text_content, strlen(text_content));
-	close(fd);
-
-	if (append_text < 0)
-		return (-1);
-	else
-		return (1);
+	if (text_content != NULL)
+	{	
+		append_text = write(fd, text_content, strlen(text_content));
+		close(fd);
+		{
+			if (append_text < 0)
+				return (-1);
+			else
+				close(fd);
+		}
+	}
+	return (1);
 }
