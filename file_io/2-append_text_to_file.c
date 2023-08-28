4f77a9ac32d278db5e7b899a8f17bbd1c5d0cd43
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-08-28 01:38:17 +0000

    delete else statement

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,14 +20,12 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	{	
+	{
 		append_text = write(fd, text_content, strlen(text_content));
 		close(fd);
 		{
 			if (append_text < 0)
 				return (-1);
-			else
-				close(fd);
 		}
 	}
 	return (1);
