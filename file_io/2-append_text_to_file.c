b0fddd807b1500fceb0889fbf180300b603a97e8
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-08-28 01:22:04 +0000

    delete flag O_WRONLY from open function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND | O_RDWR);
+	fd = open(filename, O_APPEND | O_RDWR);
 	if (fd == -1)
 		return (-1);
 
